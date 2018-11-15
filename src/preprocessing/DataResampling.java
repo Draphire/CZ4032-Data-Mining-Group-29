358e7e1357c5a5b5e489cbe4962a0220e71880f1
Draphire <Draphire@users.noreply.github.com>
2018-11-16 04:07:41 +0800

    included wrapper subset and resampling

---
+++ src/preprocessing/DataResampling.java
@@ -0,0 +1,88 @@
+package preprocessing;
+
+import java.io.File;
+
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.instance.Resample;
+import weka.filters.unsupervised.attribute.Remove;
+
+public class DataResampling {
+	public static void main(String args[]) throws Exception{
+		
+		DataSource source = new DataSource("data\\googleplaystoredata.csv");
+		
+		Instances dataset = source.getDataSet();
+		dataset.setClassIndex(dataset.numAttributes() - 1);
+		 
+		//Instances dataset = new Instances(new BufferedReader(new FileReader("data\\googleplaystoredata.csv")));
+		
+		//System.out.println(dataset.toSummaryString());
+		
+		Resample r = new Resample(); 
+		r.setNoReplacement(true); 
+		r.setSampleSizePercent(1); // or whatever % you require 
+		r.setInputFormat(dataset); 
+		  
+//		String[] opts = new String[]{ "-R", "12" };
+//		
+//		Remove remove = new Remove();
+//		
+//		remove.setOptions(opts);
+//
+//		remove.setInputFormat(dataset);
+		
+		Instances newData = Filter.useFilter(dataset, r);
+		
+//		
+//		ArffSaver saver = new ArffSaver();
+//		saver.setInstances(newData);
+//		saver.setFile(new File("data\\new.arff"));
+//		saver.writeBatch();
+	}
+	
+	public static Instances resampleData(Instances dataset){
+		
+		
+	//System.out.println(dataset.toSummaryString());
+		
+		Resample r = new Resample(); 
+		r.setNoReplacement(true); 
+		r.setSampleSizePercent(1); // or whatever % you require 
+//		try {
+//			r.setInputFormat(dataset);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
+		  
+//		String[] opts = new String[]{ "-R", "12" };
+//		
+//		Remove remove = new Remove();
+//		
+//		remove.setOptions(opts);
+//
+//		remove.setInputFormat(dataset);
+	//	Instances newData = Filter.useFilter(dataset, r);
+		Instances newData = null;
+		try {
+			r.setInputFormat(dataset);
+			
+			newData = Filter.useFilter(dataset, r);
+			newData.setClassIndex(newData.numAttributes() - 1);
+			System.out.println(newData.toSummaryString());
+			return newData;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return dataset;
+		
+	}
+	
+	
+}
---
+++ src/preprocessing/WrapperSubsetEvaluation.java
@@ -0,0 +1,130 @@
+/*
+ *  How to use WEKA API in Java 
+ *  Copyright (C) 2014 
+ *  @author Dr Noureddin M. Sadawi (noureddin.sadawi@gmail.com)
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it as you wish ... 
+ *  I ask you only, as a professional courtesy, to cite my name, web page 
+ *  and my YouTube Channel!
+ *  
+ */
+package preprocessing;
+import weka.attributeSelection.BestFirst;
+//import required classes
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GreedyStepwise;
+import weka.attributeSelection.WrapperSubsetEval;
+import weka.core.AttributeStats;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.core.converters.ArffSaver;
+import java.io.File;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.experiment.Stats;
+
+public class WrapperSubsetEvaluation{
+	public static void main(String args[]) throws Exception{
+		//load dataset
+		DataSource source = new DataSource("data\\TerrorismDataBinaryBaseline.csv");
+		Instances dataset = DataResampling.resampleData(source.getDataSet());
+		dataset.setClassIndex(dataset.numAttributes() - 1);
+		 
+		
+		//create AttributeSelection object
+		AttributeSelection filter = new AttributeSelection();
+		//create evaluator and search algorithm objects
+		WrapperSubsetEval eval = new WrapperSubsetEval();
+	//	GreedyStepwise search = new GreedyStepwise();
+		BestFirst search = new BestFirst();
+		
+		//set the algorithm to search backward
+		String[] options = new String[4];
+		options[0] = "-D";   // attribute index
+		options[1] = "0";    // 5
+		options[2] = "-N";   // match if value is smaller than
+		options[3] = "5"; 
+		//options[3] = "NA";   // 10
+//		options[2] = "-L";   // match if value is smaller than
+//		options[3] = "NA";   // 10
+		
+		search.setOptions(options);
+		//search.setSearchBackwards(true);
+		//set the filter to use the evaluator and search algorithm
+		filter.setEvaluator(eval);
+		filter.setSearch(search);
+		//specify the dataset
+		filter.setInputFormat(dataset);
+		//apply
+		Instances data = Filter.useFilter(dataset, filter);
+		//save
+		   if (data.classIndex() == -1) {
+		       data.setClassIndex(data.numAttributes() - 1);
+		    }
+		   
+		    //get number of attributes (notice class is not counted)
+			int numAttr = data.numAttributes()-1;
+			for (int i = 0; i < numAttr; i++) {
+				
+				
+				System.out.println("The "+i+ "th Attribute is " + data.attribute(i).name());
+				
+				//check if current attr is of type nominal
+//				if (data.attribute(i).isNominal()) {
+//					System.out.println("The "+i+"th Attribute is Nominal");	
+//					//get number of values
+//					int n = data.attribute(i).numValues();
+//					System.out.println("The "+i+"th Attribute has: "+n+" values");
+//					
+//				}			
+//				
+//				//get an AttributeStats object
+//				AttributeStats as = data.attributeStats(i);
+//				int dC = as.distinctCount;
+//				System.out.println("The "+i+"th Attribute has: "+dC+" distinct values");
+//				
+//				//get a Stats object from the AttributeStats
+//				if (data.attribute(i).isNumeric()){
+//					System.out.println("The "+i+"th Attribute is Numeric");	
+//				    Stats s = as.numericStats;
+//				    System.out.println("The "+i+"th Attribute has min value: "+s.min+" and max value: "+s.max+" and mean value: "+s.mean);
+//				}
+			}
+			//get number of instances
+//			int numInst = data.numInstances();
+//			int missingCountHeight = 0;
+//			//loop through all instances
+//			for (int j = 0; j < numInst; j++) {
+//				//get the j'th instance
+//				Instance instance = data.instance(j);
+//				//check if 1st attr is missing from the j'th instance
+//				if (instance.isMissing(4)) {
+//					missingCountHeight++;
+//				}
+//				
+//				
+//				
+//				//check if the class is missing from the j'th instance
+//				if (instance.classIsMissing()) {
+//					System.out.println("The class is missing in the "+j+"th Instance");		
+//				}
+//				//if you want to access the value of Class in your data
+//				//notice classes of type nominal and string are given ID's
+////				double cV = instance.classValue();
+////				System.out.println(instance.classAttribute().value((int)cV));
+//				
+//			}
+//			System.out.println("The "+4+"th has " + missingCountHeight + " Missing Values");	
+//			
+//			
+		}
+//		
+//		
+//		ArffSaver saver = new ArffSaver();
+//		saver.setInstances(newData);
+//		saver.setFile(new File("data\\TerrorismDataResampled.csv"));
+//		saver.writeBatch();
+	}
+
